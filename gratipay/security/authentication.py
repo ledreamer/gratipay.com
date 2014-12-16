5e072c869812ac110dafae54635d8590037f595d
Changaco <changaco@changaco.oy.lc>
2014-12-16 14:03:27 +0100

    fix basic auth
    fixes https://app.getsentry.com/gratipay/gratipay-com/group/45029121/

--- gratipay/security/authentication.py
+++ gratipay/security/authentication.py
@@ -1,7 +1,9 @@
 """Defines website authentication helpers.
 """
+import binascii
 from datetime import datetime
 
+from aspen import Response
 from aspen.utils import to_rfc822
 from gratipay.security import csrf
 from gratipay.security.user import User, SESSION
@@ -17,17 +19,23 @@ def get_auth_from_request(request):
     elif 'Authorization' in request.headers:
         header = request.headers['authorization']
         if header.startswith('Basic '):
-            creds = header[len('Basic '):].decode('base64')
-            token, ignored = creds.split(':')
-            user = User.from_api_key(token)
+            try:
+                creds = binascii.a2b_base64(header[len('Basic '):]).split(':', 1)
+            except binascii.Error:
+                raise Response(400, 'Malformed "Authorization" header')
+            if len(creds) != 2:
+                raise Response(401)
+            userid, api_key = creds
+            user = User.from_id(userid)
+            if user.ANON or user.participant.api_key != api_key:
+                raise Response(401)
 
             # We don't require CSRF if they basically authenticated.
             csrf_token = csrf._get_new_csrf_key()
             request.headers.cookie['csrf_token'] = csrf_token
             request.headers['X-CSRF-TOKEN'] = csrf_token
             if 'Referer' not in request.headers:
-                request.headers['Referer'] = \
-                                        'https://%s/' % csrf._get_host(request)
+                request.headers['Referer'] = 'https://%s/' % csrf._get_host(request)
     elif SESSION in request.headers.cookie:
         token = request.headers.cookie[SESSION].value
         user = User.from_session_token(token)
